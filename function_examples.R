e7757e74a152871995beb4d30f70f7aaec39d015
antoinesla <antoine.agricole@gmail.com>
2022-12-13 10:10:53 +0000

    added function examples

---
+++ function_examples.R
@@ -0,0 +1,17 @@
+# function syntax
+my_function <- function() {
+  print("hello world")
+}
+
+# calling the function
+my_function()
+
+
+# function with an argument
+my_function <- function(fname) {
+  paste(fname, "Griffin")
+}
+
+my_function("Peter")
+my_function("Lois")
+my_function("Stewie")
