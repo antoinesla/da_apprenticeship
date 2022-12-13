1068c6d936ada4f9bcb359beaab3c74ac778ef6c
antoinesla <antoine.agricole@gmail.com>
2022-12-13 12:42:42 +0000

    added a few more vector examples

--- data_types.R
+++ data_types.R
@@ -1,3 +1,20 @@
 # creating a vector
 fruits <- c("banana", "apple", "orange")
-print(fruits)
+print(fruits) # "banana", "apple", "orange"
+length(fruits) # 3
+print(sort(fruits)) # "apple"  "banana" "orange"
+print(fruits) # "banana", "apple", "orange"
+print(fruits[1]) # "banana"
+print(fruits[-1]) # "apple", "orange"
+print(fruits[length(fruits)]) # "orange"
+
+
+# sequence of numbers
+numbers1 <- 1.5:6.5
+numbers2 <- 1.5:6.3
+
+print(numbers1)
+print(numbers2)
+
+numbers <- seq(0,100,by = 20)
+numbers
