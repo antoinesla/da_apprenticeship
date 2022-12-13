74cd45f40c6830e008cf5b9c1cdcaf2a0ea4bb54
antoinesla <antoine.agricole@gmail.com>
2022-12-13 15:09:59 +0000

    added vector exercise file

---
+++ vector_exercises.R
@@ -0,0 +1,13 @@
+# first exercise
+x <- c(4,6,5,7,10,9,4,15)
+for (i in 1:length(x)) {
+  if (x[i] < 7) {
+    print("true")
+  } else {
+    print("false")
+  }
+}
+# correct answer: e.
+
+
+# second exercise
