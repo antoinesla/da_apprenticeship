884eda6b3e183991dd6d9611da214072e1a6e76e
antoinesla <antoine.agricole@gmail.com>
2022-12-14 10:31:24 +0000

    added list exercise file

---
+++ list_exercises.R
@@ -0,0 +1,42 @@
+# exercise 1
+p <- c(2,7,8)
+q <- c("A", "B", "C")
+
+x <- list(p, q)
+x[2] # "A" "B" "C"
+# answer: b.
+
+
+# exercise 2
+w <- c(2, 7, 8)
+v <- c("A", "B", "C")
+x <- list(w, v)
+x[[2]][1] <- "K"
+# answer: b.
+
+
+# exercise 3
+
+
+
+
+# exercise 1
+
+
+
+# exercise 1
+
+
+
+# exercise 1
+
+
+
+# exercise 1
+
+
+
+# exercise 1
+
+
+
