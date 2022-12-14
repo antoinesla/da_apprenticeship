3432a297186ada713e3de8bcf9c6550f839e5089
antoinesla <antoine.agricole@gmail.com>
2022-12-14 11:48:56 +0000

    added matrix example file

---
+++ matrix_examples.R
@@ -0,0 +1,29 @@
+number_matrix <- matrix(c(1,2,3,4,5,6), nrow=3, ncol=2)
+print(number_matrix)
+
+string_matrix <- matrix(c("apple","banana","cherry","orange"), nrow=2, ncol=2)
+print(string_matrix)
+
+# accessing elements
+number_matrix[1,2]
+number_matrix[1,]
+number_matrix[,2]
+number_matrix[c(1,2),]
+number_matrix[1:2,]
+
+# adding columns and rows
+number_matrix <- cbind(number_matrix, 1:3) # adding a column with 1, 2, 3
+print(number_matrix)
+number_matrix <- rbind(number_matrix, c(45,50,60))
+print(number_matrix)
+
+
+# removing rows and columns
+number_matrix <- number_matrix[-c(1),] # removing the first row
+print(number_matrix)
+number_matrix <- number_matrix[,-c(2)] # removing the second row
+print(number_matrix)
+
+
+# dim function
+dim(number_matrix)
